012f58561f0062216c1d21cf8162aecb1f90dd92
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-27 14:36:44 -0300

    ci: loading env

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Env.Load();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
