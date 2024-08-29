9cefd3fee3d7f934bf9370ff73e372a04c28c7d9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-08-28 21:22:20 -0300

    chore: sending request at llama service

--- Controllers/PromptController.cs
+++ Controllers/PromptController.cs
@@ -9,13 +9,16 @@ namespace llama_api.Controllers
     [ApiController]
     public class PromptController : ControllerBase
     {
+        private readonly LlamaRequest _service = new();
+        
         [HttpPost]
         [Tags("Send a Message")]
         public async Task<IActionResult> Message([FromBody] PromptDTO dto) 
         {
+            
             try 
             {
-                var message = await LlamaRequest.Prompt(dto.Message);
+                var message = await _service.Prompt(dto.Message);
                 return Ok(new PromptDTO { Message = message });
             } 
             catch (InvalidDataException) 
--- Services/LlamaRequest.cs
+++ Services/LlamaRequest.cs
@@ -1,16 +1,23 @@
 using System;
+using System.Net.Http.Headers;
 using System.Text.Json.Nodes;
-using GroqApiLibrary;
 
 namespace llama_api.Services;
 
 public class LlamaRequest
 {
-    public static async Task<string> Prompt(string prompt) 
+    private readonly HttpClient _http = new();
+    private const string Url = "https://api.groq.com/openai/v1/chat/completions";
+
+    public LlamaRequest()
     {
         var apiKey = Environment.GetEnvironmentVariable("API_KEY") ?? throw new InvalidDataException("API_KEY is not set");
-        var client = new GroqApiClient(apiKey);
 
+        _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+    }
+
+    public async Task<string> Prompt(string prompt) 
+    {
         var request = new JsonObject
         {
             ["model"] = "llama3-8b-8192",
@@ -24,8 +31,18 @@ public class LlamaRequest
             }
         };
 
-        var response = await client.CreateChatCompletionAsync(request);
-        var message = response?["choices"]?[0]?["message"]?["content"]?.ToString();
+        var response = await _http.PostAsJsonAsync(Url, request);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException($"API request failed with status code {response.StatusCode}. Response content: {errorContent}");
+        }
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var jsonResponse = JsonNode.Parse(responseBody);
+
+        var message = jsonResponse?["choices"]?[0]?["message"]?["content"]?.ToString();
 
         return message ?? "";
     }
